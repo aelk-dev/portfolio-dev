0403b67f0b30d19d7570fde321c210aa39be8f64
Mani-tech7 <magnolia-hibi@proton.me>
2026-05-08 00:31:48 +0000

    refactor: projects data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,18 +1,27 @@
 export const projects = [
   {
     title: "Portfolio Website",
-    description:
-      "Modern full-stack portfolio with animations and responsive design.",
+    description: "Modern portfolio with animations and responsive design.",
     tech: ["Javascript", "React", "TailwindCSS", "Framer Motion"],
     github: "https://github.com/aelk-dev/portfolio-dev",
     demo: "http://foliodev-aya.netlify.app/",
   },
   {
-    title: "Full Stack App",
+    title: "Job Tracker - 🚧 This project is currently under development.",
     description:
-      "Application with authentication, API integration and database.",
-    tech: ["none"],
-    github: "#",
-    demo: "#",
+      "A complete job tracking platform allowing users to log applications, monitor status updates, and streamline their job search workflow with a clean and efficient interface.",
+    tech: [
+      "TypScript",
+      "Next.js",
+      "Bootstrap",
+      "Java",
+      "Spring Boot",
+      "PostgresSQL",
+      "Jest",
+      "Playwright",
+      "Docker/Docker-compose",
+    ],
+    github: "/",
+    demo: "/",
   },
 ];
